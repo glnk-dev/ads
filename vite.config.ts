a9c2c21f655db43948d08085585ddbcc3108efc6
Sangjun Son <sangjun.son@moloco.com>
2025-12-25 20:06:51 -0800

    chore: set CNAME to use _ads.glnk.dev

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages base path - update to match your repo name
-  base: '/ads/',
+  base: '/',
 })
